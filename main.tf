6548045db32eeee8e5ed15ee959f6b96dd890125
sunpyopark <59463698+sunpyopark@users.noreply.github.com>
2020-11-22 10:28:52 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-bucket-sun"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-1"
   }
 }
 
